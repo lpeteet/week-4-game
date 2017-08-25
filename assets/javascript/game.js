29a4e96355672dfd3b823801a635716aa268fafb
Larry Peteet <larrypv10@gmail.com>
2017-08-25 09:18:02 -0400

    Good Start for Screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,22 @@
-/* Global Variables??? */
+/* Global Variables */
+var wins = 0;
+var losses = 0;
+var scoreToMatch = 0;
+var theirScore = 0;
+
+//jQuery Click Events
+$(document).ready(function(){
+    $("img").click(function(){
+    alert("It Works and ID: " + $(this).attr('id'));
+   });
+});
+
+// $(document).ready(function(){
+//     $("p").click(function(){
+//         alert("The paragraph was clicked.");
+//     });
+//     $("#img1").click(function() {
+//         alert( "Handler for .click() #img1 called." );
+//     });
+// });
+
